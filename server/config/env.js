b9c22d5d37f1942e8a9f9ea11f09c8937b58a6ff
baozdemir <barisozdemir54@gmail.com>
2017-11-15 15:52:42 +0300

    comment line added for .env intelligibility

--- server/config/env.js
+++ server/config/env.js
@@ -3,13 +3,13 @@
 require('dotenv').config();
 
 module.exports = {
-  PORT: process.env.PORT || 8080,
+  PORT: process.env.PORT || 8080, // test port
   databases: {
     main: {
-      DATABASE_NAME: process.env.DATABASE_NAME || "dbname",
-      DATABASE_HOST: process.env.DATABASE_HOST || "host",
-      DATABASE_USERNAME: process.env.DATABASE_USERNAME || "username",
-      DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || "password",
+      DATABASE_NAME: process.env.DATABASE_NAME || "dbname",           // test dbname
+      DATABASE_HOST: process.env.DATABASE_HOST || "host",             // test host
+      DATABASE_USERNAME: process.env.DATABASE_USERNAME || "username", // test username
+      DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || "password", // test password
       DATABASE_DIALECT: process.env.DATABASE_DIALECT || "mysql",
       NODE_ENV: process.env.NODE_ENV || "development"
     }
